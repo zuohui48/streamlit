403862cab6a9bddf2785d045d25669f69435595c
zuohui48 <83281769+zuohui48@users.noreply.github.com>
2024-03-29 17:03:36 +0800

    add search bar

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
-def main():
-    st.title('My Streamlit App')
-    st.write('Welcome to my Streamlit app!')
+st.title("WOW DATA JOBS")
+with st.expander('About this app'):
+  st.markdown('**What can this app do?**')
+  st.info('This app shows job seekers on some good statistics on data/software related jobs.')
+  st.markdown('**How to use the app?**')
+  st.info('To engage with the app,\n'
+             '1. Select which company u want to see then u will see some good stats \n'
+             '2. Select the job u want to apply and we will show u some GOOD jobs')
 
-if __name__ == '__main__':
-    main()
+text_search = st.text_input("Search Company", value="")

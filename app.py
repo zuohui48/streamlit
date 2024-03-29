5891000d2b40c6b096aa007f611ab1d049948642
loqir <ng.chekkhau@gmail.com>
2024-03-29 16:36:07 +0800

    try setup

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import streamlit as st
+from pymongo import MongoClient
 
 def main():
     st.title('My Streamlit App')
     st.write('Welcome to my Streamlit app!')
+
+    # @st.experimental_singleton(suppress_st_warning=True)
+    def init_connection():
+        return MongoClient("mongodb+srv://st.secrets.db_username:st.secrets.db_pswd@st.secrets.cluster_name.n4ycr4f.mongodb.net/?retryWrites=true&w=majority")
+
+    client = init_connection()
+    
+    # @st.experimental_memo(ttl=60)
+    def get_data():
+        db = client.indeed #establish connection to the 'sample_guide' db
+        items = db.planets.find() 
+        items = list(items)        
+        return items
+    
+    data = get_data()
+
+    for item in data:
+        st.write(item)
 
 if __name__ == '__main__':
     main()

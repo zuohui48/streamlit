547fd27f24b8165b0e90d3a8860a8962960a4589
zuohui48 <83281769+zuohui48@users.noreply.github.com>
2024-04-02 15:04:02 +0800

    remove container in front page

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 
 st.title("WOW WELCOME TO DATA JOBS TIME TO FIND MASSIVE MEGA GOOT STRADEJ")
-with st.expander('About this app'):
-  st.markdown('**What can this application do?**')
-  st.info('1. This application provides job seekers statistics on data/software related jobs according to recent industry trends. \n'
+st.header('About this app')
+st.markdown('**What can this application do?**')
+st.info('1. This application provides job seekers statistics on data/software related jobs according to recent industry trends. \n'
           '2. This application provides job seekers statistics on relevant companies for them to better assess their job opportunities. \n'
           '3. This application provides job seekers with recommended similar jobs based on selected jobs')
-  st.markdown('**How to use the app?**')
-  st.info('To engage with the app,\n'
+st.markdown('**How to use the app?**')
+st.info('To engage with the app,\n'
              '1. Proceed to the company statistics page to view statistics of desired company \n'
              '2. Proceed to the job statistics page to view statistics of data related jobs \n'
              '3. Select the job u want to apply and we will recommend you some similar GOOD jobs')
-
-
 
 

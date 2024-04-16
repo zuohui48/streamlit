3ae5c29d8d50cd1c2f64e0685b5d7ae2a3c96db0
loqir <ng.chekkhau@gmail.com>
2024-04-16 22:48:23 +0800

    tech stack statistics

--- pages/Tech Stack Statistics.py
+++ pages/Tech Stack Statistics.py
@@ -5,6 +5,7 @@ import certifi
 import matplotlib.pyplot as plt
 import numpy as np
 from st_pages import Page, Section, show_pages, add_page_title
+from collections import Counter
 
 # Either this or add_indentation() MUST be called on each page in your
 # app to add indendation in the sidebar
@@ -31,11 +32,41 @@ def get_hardSkills_data():
 
 data = get_hardSkills_data()
 
-data_science = data[data["mainJob"] == "data science"]
-skills = []
 
-for i in data_science["hardSkills"]:
-  st.write(f"i {i}")
-  for j in i.split(","):
-    lst = j.split(" ")
-    st.write(lst)
+job_skills = {}
+for mainjob in data["mainJob"].unique():
+  filtered = data[data["mainJob"] == mainjob]
+  for i in filtered["hardSkills"]:
+    k = i.split(",")
+    for j in k:
+      l = j.split(" ")
+      if len(l) <= 2:
+        skill = "".join(l).lower()
+        if skill != "none":
+          if mainjob not in job_skills:
+            job_skills[mainjob] = [skill]
+          else:
+            curr_list = job_skills[mainjob]
+            curr_list.append(skill)
+            job_skills[mainjob] = curr_list
+
+for mainjob, skills_list in job_skills.items():
+  st.write(mainjob)
+  top_skills = list(Counter(skills_list).most_common(5))
+  skills = []
+  count = []
+  for i in top_skills:
+    skills.append(i[0])
+    count.append(i[1])
+  # Plotting the pie chart
+  fig, ax = plt.subplots()
+  ax.pie(count, labels=skills, autopct='%1.1f%%', startangle=140)
+  ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+  ax.set_title(f"Most Common Tech Stack for {mainjob}")
+
+  # Display the pie chart
+  st.pyplot(fig)
+
+
+
+
